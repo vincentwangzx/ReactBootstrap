e919b11c4b907d09bacb5374bb96598af55f83ed
vincentwangzx <vincent.nicodemus@gmail.com>
2020-11-18 21:41:45 +0800

    Completed navbar

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import Poster from '../assets/Poster.jpg';
+
+export const Jumbotron = () => ()
