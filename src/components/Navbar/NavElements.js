8c6db812941d1cc0659eac07b72022fde94c45bd
vincentwangzx <vincent.nicodemus@gmail.com>
2020-11-18 21:38:35 +0800

    built navbar component

--- src/components/Navbar/NavElements.js
+++ src/components/Navbar/NavElements.js
@@ -1,51 +1,119 @@
 import styled from 'styled-components';
-import { Link as LinkR } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 
 export const Nav = styled.nav`
     background: #ff5c7a; 
-    height: 50px;
+    height: 68px;
     display: flex; 
-    justify-content: center; 
+    justify-content: space-between; 
     align-items: center; 
     font-size: 1rem;
     position: sticky;
     top: 0; 
-    z-index: 10;
+    z-index: 1;
 
-    @media screen and (max-width: 960px) {
+    @media screen and (max-width: 1100px) {
         transition: 0.8s all ease;
     }
 `;
 
 export const NavbarContainer = styled.div`
     display: flex; 
-    height: 50px; 
+    height: 68px; 
     z-index: 1; 
     width: 100%; 
     padding: 0 24px; 
-    align-content: right;
+    text-align: center;
     max-width: 1100px; 
+    
+`
 
-    .nav-link {
-        color: white !important; 
-        &:hover {
-            color: palevioletred;
-        }
-    }
-`;
-
-export const NavLogo = styled(LinkR)` 
+export const NavLogo = styled(Link)` 
     
     justify-self: flex-start;
-    cursor: pointer;
     display: flex; 
-    align-items: center; 
     text-decoration: none; 
-
-    .img-fluid {
-        width: 160px;
-        height: 44px;
-        position: absolute;
-
-    }
+    box-sizing: border-box;
+    max-width: 160px;
+    margin-right: 0.5rem;
+    align-items: left; 
 `;
+
+export const MobileIcon = styled.div`
+display: none;
+
+
+@media screen and (max-width: 960px) {
+    display: block; 
+    position: absolute; 
+    top: 0;
+    right: 0; 
+    transform: translate(-100%, 50%);
+    font-size: 1.8rem;
+    cursor: pointer; 
+}
+`;
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center; 
+    list-style: none;
+    text-align: center; 
+
+    @media screen and (max-width: 960px) {
+        display: flex; 
+        flex-direction: column;
+        width: 100%;
+        height: 90vh;
+        position: absolute; 
+        top: 68px; 
+        left: ${({click}) => (click ? 0 : '-100%')};
+        opacity: 0.85;
+        transition: all 0.5s ease;
+        background: #ff5c7a;
+    }
+
+`
+export const NavItem = styled.li`
+    height: 80px;
+    border-bottom: 2px solid transparent;
+
+    &:hover {
+        border-bottom: 2px solid lightpink;
+    }
+
+    @media screen and (max-width: 960px) {
+        width: 100%;
+
+        &:hover {
+            border: none;
+        }
+    }
+`
+
+export const NavLink = styled(Link)`
+    color: #fff;
+    display: flex;
+    align-items: center;
+    text-decoration: none; 
+    padding: 0.5rem 1rem;
+    height: 100%;
+    font-weight: bold;
+
+    &:hover {
+        color: lightpink;
+    }
+
+    @media screen and (max-width: 960px) {
+        text-align: center; 
+        padding: 1rem; 
+        width: 100%; 
+        display: table; 
+
+        &:hover {
+            color: lightpink;
+            transition: all 0.3s ease;
+        }
+    }
+`
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,22 +1,41 @@
-import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavElements';
-import { NavItem } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import logo from './logo.png'
+import React, { useState } from 'react'
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLink } from './NavElements';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import logo from './logo.svg';
+
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
     return (
         <React.Fragment>
-            <Nav className="ml-auto">
-                 <NavbarContainer>
-                      <NavLogo className="nav-logo" to='/'><img src={logo} alt="Logo" class="img-fluid"></img></NavLogo>
-                    <NavItem><Link className="nav-link" to="/">Home</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/delivery">Book A Delivery</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/join-us">Join Us</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/contact-us">Contact Us</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/faq">FAQ</Link></NavItem>
+            <IconContext.Provider value={{ color: '#fff'}}>
+            <Nav >
+            <NavLogo className="nav-logo" to='/'><img src={logo} alt="Logo" className="img-fluid"></img></NavLogo>
+            <NavbarContainer>
+            <MobileIcon onClick={handleClick}>
+                {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click} className="ml-auto">
+                    <NavItem><NavLink className="nav-link" to="/">Home</NavLink></NavItem>
+                    <NavItem><NavLink className="nav-link"to="/delivery">Book A Delivery</NavLink></NavItem>
+                    <NavItem><NavLink className="nav-link" to="/join-us">Join Us</NavLink></NavItem>
+                    <NavItem><NavLink className="nav-link" to="/contact-us">Contact Us</NavLink></NavItem>
+                    <NavItem><NavLink className="nav-link" to="/faq">FAQ</NavLink></NavItem>
+            </NavMenu>
                 </NavbarContainer>
             </Nav>
+            </IconContext.Provider>
         </React.Fragment >
     )
 }
